0753e7b8c069d73bd9ec36bb530f0033a19d538d
tuyishimehono <tuhonori1@gmail.com>
2024-06-28 15:19:33 +0200

    refactor fetchWeather.js

--- exercises/fetchWeather.js
+++ exercises/fetchWeather.js
@@ -1,22 +1,26 @@
 async function fetchWeather(country) {
-    try {
-        const countryDetails = await fetch(
-            `https://restcountries.com/v3.1/name/${country}`
-          );
-          const response = await countryDetails.json()
-          const location = response[0].capitalInfo.latlng;
-          const weather = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${location[0]}&longitude=${location[1]}&current_weather=true`
-          );
-          const weatherResponse = await weather.json();
-          console.log(`Country: ${response[0].name.common}
-        City: ${response[0].capital}
-        Temperature: ${weatherResponse.current_weather.temperature} ${weatherResponse.current_weather_units.temperature}`);
-    } catch (error) {
-        console.log('Error: ', error)
-    }
-}
-fetchWeather('France');
-fetchWeather('Burundi');
-fetchWeather('Rwanda');
+  try {
+    const countryDetails = await fetch(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
+    if (!countryDetails.ok) throw new Error("failed to fetch");
 
+    const response = await countryDetails.json();
+    const location = response[0].capitalInfo.latlng;
+    const weather = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${location[0]}&longitude=${location[1]}&current_weather=true`
+    );
+    if (!weather.ok) throw new Error("failed to fetch");
+
+    const weatherResponse = await weather.json();
+    console.log(`
+          Country: ${response[0].name.common}
+          City: ${response[0].capital}
+          Temperature: ${weatherResponse.current_weather.temperature} ${weatherResponse.current_weather_units.temperature}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchWeather("France");
+fetchWeather("Burundi");
+fetchWeather("Rwanda");
